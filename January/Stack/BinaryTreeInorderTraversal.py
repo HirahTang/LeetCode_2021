8ee9f64c62e64e8772b7ca0a4b1254e23fe8a93d
Han <tanghan1995@gmail.com>
2021-01-28 15:49:46 +0800

    Add files via upload

---
+++ January/Stack/BinaryTreeInorderTraversal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 15:34:42 2021
+
+@author: hirahtang
+"""
+
+
+# =============================================================================
+# 94. Binary Tree Inorder Traversal - stack approach
+# =============================================================================
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()  # the last element
+            if node:
+                if visited:
+                    res.append(node.val)
+                else:  # inorder: left -> root -> right
+                    stack.append((node.right, False))
+                    stack.append((node, True))
+                    stack.append((node.left, False))
+        return res
